212580ba120bfc894c386302a010c4ef5ac33f25
Mike Robinson <michael_robinson@live.ca>
2021-08-08 09:44:50 -0400

    first commit

---
+++ channel.lua
@@ -0,0 +1,8 @@
+-- Some attempt at a Lua script
+
+
+-- Use the page name as a reference for the MIDI channel
+-- Set in a Pager control
+MIDI_CHANNEL = tonumber(control.properties.page)
+
+control:setValueProperty(, property, value)
